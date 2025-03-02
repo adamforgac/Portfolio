506b37bb5a61c85a6ba6f695b8e7999d9556b558
adamforgac <aforg@email.cz>
2025-03-02 22:00:10 +0100

    Add script for loading off-screen images lazy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 // Core scripts
+
+// Přidá na telefonních zařízeních lazy load obrázkům, které mají classu lazy-load
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth <= 768) {
+    // Adjust the breakpoint for mobile
+    document.querySelectorAll('img.lazy-load').forEach((img) => {
+      img.setAttribute('loading', 'lazy');
+    });
+  }
+});
